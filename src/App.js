7f1caec96f4d0d5e4cbc2caa39541811a847198c
Chris Kim <ckjr92@gmail.com>
2021-12-04 16:48:56 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="questionClass">
+        <h1>This is a Question</h1>
+      </div>
+      <div className="button-overall">
+        <button className="normal-button">Button</button>
+        <button>Button</button>
+        <button>Button</button>
+        <button>Button</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
